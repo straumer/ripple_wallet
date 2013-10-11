04d24dce7696e48a67789b3f1dbad1c28786ddab
Matthías Ragnarsson <mar17@hi.is>
2013-10-11 18:24:56 +0000

    Added a button event that prints to system log.

---
+++ ripple_wallet/src/org/icehat/ripplewallet/LogIn.java
@@ -0,0 +1,21 @@
+package org.icehat.ripplewallet;
+
+import javax.websocket.ClientEndpoint;
+import android.util.Log;
+
+/** Handles the process of logging into wallet by talking
+ *  to server.
+ *
+ *  @author Matthías Ragnarsson
+ */
+public class LogIn {
+
+    private static final String TAG = "RippleLogIn";
+
+    /** Prints some information to system log.\ Its purpose is
+     *  for checking strings sent from server.
+     */
+    public void printToLog() {
+       Log.i(TAG, "Testi!"); 
+    }
+}
--- ripple_wallet/src/org/icehat/ripplewallet/RippleWallet.java
+++ ripple_wallet/src/org/icehat/ripplewallet/RippleWallet.java
@@ -2,13 +2,33 @@ package org.icehat.ripplewallet;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+/** Starting activity with a log in screen. 
+ *  
+ *  @author Matthías Ragnarsson
+ */
 public class RippleWallet extends Activity
 {
+    private LogIn li = new LogIn();
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+
+    /** Logs a user into a wallet with an address and password
+     *  specified in the activity's address and password fields.\ When
+     *  that occurs, appropriate information is stored and the activity
+     *  switches to wallet's balance activity.\ If the address or password 
+     *  are invalid, an error message is displayed on screen.
+     *
+     *  Note: Method in the making, doing tests.
+     */
+    public void logIn(View view) {
+        this.li.printToLog();
     }
 }
