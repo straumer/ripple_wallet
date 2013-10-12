079761cf7c524eb306c05b48693b810a3cbb2899
Matthías Ragnarsson <mar17@hi.is>
2013-10-12 17:04:28 +0000

    Added the tyrus-client and dependencies to deploy websocket endpoint.

--- ripple_wallet/src/org/icehat/ripplewallet/LogIn.java
+++ ripple_wallet/src/org/icehat/ripplewallet/LogIn.java
@@ -1,6 +1,7 @@
 package org.icehat.ripplewallet;
 
 import javax.websocket.ClientEndpoint;
+import org.glassfish.tyrus.client.ClientManager;
 import android.util.Log;
 
 /** Handles the process of logging into wallet by talking
