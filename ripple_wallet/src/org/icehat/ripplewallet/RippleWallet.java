871767c1b5f4e5592f9744add78e1e6e790783e1
Matthías Ragnarsson <mar17@hi.is>
2013-10-13 16:09:18 +0000

    Added websocket client to login button.

--- ripple_wallet/src/org/icehat/ripplewallet/RippleWallet.java
+++ ripple_wallet/src/org/icehat/ripplewallet/RippleWallet.java
@@ -3,12 +3,16 @@ package org.icehat.ripplewallet;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.io.IOException;
+import java.util.List;
+import java.lang.Integer;
+import org.apache.http.message.BasicNameValuePair;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import com.codebutler.android_websockets.WebSocketClient;
+import com.codebutler.android_websockets.HybiParser;
 
 /** Starting activity with a log in screen. 
  *  
@@ -17,6 +21,9 @@ import com.codebutler.android_websockets.WebSocketClient;
 public class RippleWallet extends Activity
 {
     private static final String TAG = "RippleWallet";
+    private static final String rippleServerURI = "wss://s1.ripple.com";
+    private WebSocketClient client;
+    List<BasicNameValuePair> extraHeaders;
 
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -36,7 +43,32 @@ public class RippleWallet extends Activity
     public void logIn(View view) {
         
         Log.i(TAG, "Button clicked");
-        
-        final String rippleServerURI = "wss://s1.ripple.com";
+
+        client = new WebSocketClient(URI.create(rippleServerURI), new WebSocketClient.Listener() {
+            @Override
+            public void onConnect() {
+                Log.d(TAG, "Connected!");
+            }
+
+            @Override
+            public void onMessage(String message) {
+                Log.d(TAG, String.format("Got string message! %s", message));
+            }
+
+            @Override
+            public void onMessage(byte[] data) {
+                Log.d(TAG, "Got binary message!");
+            }
+
+            @Override
+            public void onDisconnect(int code, String reason) {
+                Log.d(TAG, String.format("Disconnected! Code: %d Reason: %s", code, reason));
+            }
+
+            @Override
+            public void onError(Exception error) {
+                Log.e(TAG, "Error!", error);
+            }
+        }, extraHeaders);
     }
 }
