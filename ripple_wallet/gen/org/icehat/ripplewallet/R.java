620216ae634f7cbdbede13519d886e44a9092982
pki1 <pki1@hi.is>
2013-11-24 16:54:56 +0000

    Program did not run, trying to commit R.java

--- ripple_wallet/gen/org/icehat/ripplewallet/R.java
+++ ripple_wallet/gen/org/icehat/ripplewallet/R.java
@@ -14,26 +14,41 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo=0x7f020001;
+        public static final int drawer_shadow=0x7f020000;
+        public static final int ic_action_about=0x7f020001;
+        public static final int ic_action_help=0x7f020002;
+        public static final int ic_action_location_found=0x7f020003;
+        public static final int ic_action_refresh=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_action_settings=0x7f020006;
+        public static final int ic_drawer=0x7f020007;
+        public static final int ic_launcher=0x7f020008;
+        public static final int logo=0x7f020009;
     }
     public static final class id {
-        public static final int account=0x7f060001;
-        public static final int balance_view=0x7f060002;
-        public static final int btnSend=0x7f06000a;
-        public static final int editText1=0x7f060007;
-        public static final int editText2=0x7f060008;
-        public static final int imageView1=0x7f060000;
-        public static final int login_message=0x7f060005;
-        public static final int passphrase=0x7f060004;
-        public static final int spinner1=0x7f060009;
-        public static final int textView1=0x7f060006;
-        public static final int wallet_name=0x7f060003;
+        public static final int account=0x7f070001;
+        public static final int balance=0x7f07000d;
+        public static final int balance_view=0x7f070002;
+        public static final int btnSend=0x7f07000a;
+        public static final int editText1=0x7f070007;
+        public static final int editText2=0x7f070008;
+        public static final int imageView1=0x7f070000;
+        public static final int login_message=0x7f070005;
+        public static final int logout=0x7f07000e;
+        public static final int passphrase=0x7f070004;
+        public static final int receive=0x7f07000c;
+        public static final int send=0x7f07000b;
+        public static final int spinner1=0x7f070009;
+        public static final int textView1=0x7f070006;
+        public static final int wallet_name=0x7f070003;
     }
     public static final class layout {
         public static final int balance=0x7f030000;
         public static final int main=0x7f030001;
         public static final int send=0x7f030002;
+    }
+    public static final class menu {
+        public static final int activity_main_actions=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
